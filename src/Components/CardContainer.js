7012be23351d89182e643fd8dcd3329c627ab347
Brian Bissette <bbissette3@yahoo.com>
2023-03-24 11:17:57 -0500

    website components

---
+++ src/Components/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WorkoutCard from './WorkoutCard';
+
+const CardContainer = () => {
+  return (
+    <div>
+        CardContainer
+        <WorkoutCard />
+    </div>
+  )
+}
+
+export default CardContainer;
---
+++ src/Components/FilterWorkouts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterWorkouts = () => {
+  return (
+    <div>FilterWorkouts</div>
+  )
+}
+
+export default FilterWorkouts;
