45a0bfc47f450f53cfd6a9f434f575ae5b3487d4
Brian Bissette <bbissette3@yahoo.com>
2023-03-24 17:17:02 -0500

    css for cards, and began filter component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import Header from "./Components/Header";
 import CardContainer from "./Components/CardContainer";
 import Footer from "./Components/Footer";
 import FilterWorkouts from "./Components/FilterWorkouts";
+import "./index.css"
 
 function App() {
-  const [workoutDataArray, setWorkoutDataArray] = useState([])
+  const [workoutDataArray, setWorkoutDataArray] = useState([]);
+  //filter state
+  const [selected, setSetlected] = useState("All");
 
   useEffect(() => {
     fetch('http://localhost:3001/Workouts')
       .then((response) => response.json())
       .then((workoutData) => setWorkoutDataArray(workoutData))
   }, [])
+
+  const categories = ["All", "back", "cardio", "chest", "lower arms", "lower legs", "neck", "shoulders", "upper arms", "upper legs", "waist"]
+
+  const workOutsToShow = workoutDataArray.filter((el) => (
+    selected === "All" || el.bodyPart === selected
+  ));
   
   return (
     <div className="App">
       <Header />
-      <CardContainer workoutDataArray={workoutDataArray}/>
-      <FilterWorkouts workoutDataArray={workoutDataArray}/>
+      <FilterWorkouts 
+        workoutDataArray={workoutDataArray} 
+        categories={categories} 
+        selected={selected} 
+        setSetlected={setSetlected}
+      />
+      <CardContainer workoutDataArray={workOutsToShow}/>
       <Footer />
     </div>
   );
