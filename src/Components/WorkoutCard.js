b65d7f019122c823b1112aa019d92e3fdf80b5bf
Timothy <sanohoang@gmail.com>
2023-03-24 15:47:44 -0500

    adding button

--- src/Components/WorkoutCard.js
+++ src/Components/WorkoutCard.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-const WorkoutCard = ({workoutDetail}) => {
-  
-  const { bodyPart, equipment, gifUrl, name, target } = workoutDetail
+const WorkoutCard = ({ workoutDetail }) => {
+  const { bodyPart, equipment, gifUrl, name, target } = workoutDetail;
 
   return (
-  <Card>
-    <Image src={gifUrl} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{bodyPart} : {target}</span>
-      </Card.Meta>
-      <Card.Description>
-        Eqipment needed: {equipment}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-  )
-  }
+    <Card>
+      <Image src={gifUrl} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>
+          <span className="date">
+            {bodyPart} : {target}
+          </span>
+        </Card.Meta>
+        <Card.Description>Eqipment needed: {equipment}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+        <a>
+          <button onClick={() => console.log("This is amazing!")}>♡</button>
+        </a>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default WorkoutCard;
