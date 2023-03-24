a4acb48e209790f1b3c0d5a4944cc4a3a43b449b
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-03-24 13:30:11 -0700

    edit

--- src/Components/WorkoutCard.js
+++ src/Components/WorkoutCard.js
@@ -1,25 +1,3 @@
-
-// const WorkoutCard = ({workoutDetail}) => {
-
-//   const { bodyPart, equipment, gifUrl, name, target } = workoutDetail
-
-//   return (
-//     <div className='workout'>WorkoutCard
-//       <img src = {gifUrl} />
-//       <div className='excercise-info'>
-//         <h3>{name}</h3>
-//         <h4>
-//           {bodyPart} : 
-//           {target}
-//         </h4>
-//         <h5> Eqipment needed: {equipment}</h5>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default WorkoutCard;
-
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
