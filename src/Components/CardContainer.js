96a801138f346a4e26c4f1ff96a1f2f16c0ae0ff
Brian Bissette <122562881+bbissette3@users.noreply.github.com>
2023-03-26 15:07:17 -0500

    Revert "Tim h"

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -1,24 +1,18 @@
-import React from "react";
-import WorkoutCard from "./WorkoutCard";
+import React from 'react';
+import WorkoutCard from './WorkoutCard';
 
-const CardContainer = ({ workoutDataArray, handleFav }) => {
+const CardContainer = ({workoutDataArray}) => {
+
   const workoutDetails = workoutDataArray.map((workoutDetail) => {
-    return (
-      <WorkoutCard
-        key={workoutDetail.id}
-        workoutDetail={workoutDetail}
-        handleFav={handleFav}
-      />
-    );
-  });
+    return <WorkoutCard key={workoutDetail.id} workoutDetail={workoutDetail}/>
+  })
 
   return (
+      <div className="workout-list"> 
+          {workoutDetails}
+      </div>
+  )
 
-    <>
-      <h2>Card Container</h2>
-      <div className="workout-list">{workoutDetails}</div>
-    </>
-  );
-};
+}
 
 export default CardContainer;
