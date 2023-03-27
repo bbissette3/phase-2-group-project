c966907944a3520d20bc17d6417b8dc0c49c012a
Timothy <sanohoang@gmail.com>
2023-03-27 13:42:46 -0500

    changes

--- src/Components/WorkoutCard.js
+++ src/Components/WorkoutCard.js
@@ -3,39 +3,21 @@ import { Card, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const WorkoutCard = ({ workoutDetail, favWorkouts }) => {
-  const { id, bodyPart, equipment, gifUrl, name, target, image } = workoutDetail;
+  const { id, bodyPart, equipment, gifUrl, name, target, image } =
+    workoutDetail;
 
   return (
     <Link to={`/workouts/${id}`}>
-    <div className="card-info">
+      <div className="card-info">
         <div className="workout-card">
           <Card>
             <Image src={gifUrl} wrapped ui={false} />
             <Card.Content>
-              <Card.Header>
-                {name}
-                <Button
-                  className="heart icon"
-                  variant="contained"
-                  style={{ float: "right" }}
-                  color="primary"
-                  position="float-right"
-                  onClick={() => favWorkouts(workoutDetail)}
-                >
-                  Save
-                </Button>
-              </Card.Header>
-              <Card.Meta>
-                <span className="date">
-                  {bodyPart} : {target}
-                </span>
-              </Card.Meta>
-              <Card.Description>Equipment needed: {equipment}</Card.Description>
-              <Image src={image} size="small" />
+              <Card.Header>{name}</Card.Header>
             </Card.Content>
           </Card>
         </div>
-    </div>
+      </div>
     </Link>
   );
 };
