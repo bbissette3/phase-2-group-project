59cc84c801aa6af9024f0b5257c6d783ae7ca9c7
Timothy <sanohoang@gmail.com>
2023-03-27 11:08:47 -0500

    added fav button

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import WorkoutCard from './WorkoutCard';
+import React from "react";
+import WorkoutCard from "./WorkoutCard";
 
-const CardContainer = ({workoutDataArray}) => {
-
+const CardContainer = ({ workoutDataArray, favWorkouts }) => {
   const workoutDetails = workoutDataArray.map((workoutDetail) => {
-    return <WorkoutCard key={workoutDetail.id} workoutDetail={workoutDetail}/>
-  })
+    return (
+      <WorkoutCard
+        key={workoutDetail.id}
+        workoutDetail={workoutDetail}
+        favWorkouts={favWorkouts}
+      />
+    );
+  });
 
-  return (
-      <div className="workout-list"> 
-          {workoutDetails}
-      </div>
-  )
-
-}
+  return <div className="workout-list">{workoutDetails}</div>;
+};
 
 export default CardContainer;
