eccca83b45ba7453804e29a649fbeefb44b3597a
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-03-27 14:20:00 -0700

    button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ import {
     Button,
     Form,
     Select,
+    Icon
 } from 'semantic-ui-react'
 
 const options = [
@@ -28,23 +29,26 @@ class Home extends Component {
         const { name, email, option} = this.state
     return (
         <div className="home" >
-             <Segment centered>
-                <Image src={logo} size='large' circular centered/>
-                <h2>About Us</h2>
-                <p>Our gym was created by 3 extreme finess competitors 
-                    who grew a passion for helping other people reach their
-                    goals too! We offer group training classes, personal 
-                    training sessions and we now have an online based training, 
-                    so you can get swoll in the comfort of your own home! 
-                    Each of our trainers have years of expierence and their own 
-                    unique specialties and training styles, giving our clients
-                    a variety of different workouts to choose from. We take pride 
-                    in our training and ability to work one-on-one with clients 
-                    to give them the best diet and exercise routines that work with 
-                    their indivual lifestyles and body types. Sign up down below for 
-                    a consultation and recieve your first month with us completely free!
-                </p>
-             </Segment>
+            <div className='home-bio'>
+                <Segment centered>
+                    <Image src={logo} size='large' circular centered/>
+                    <h2>About Us</h2>
+                    <p>Our gym was created by 3 extreme finess competitors 
+                        who grew a passion for helping other people reach their
+                        goals too! We offer group training classes, personal 
+                        training sessions and we now have an online based training, 
+                        so you can get swoll in the comfort of your own home! 
+                        Each of our trainers have years of expierence and their own 
+                        unique specialties and training styles, giving our clients
+                        a variety of different workouts to choose from. We take pride 
+                        in our training and ability to work one-on-one with clients 
+                        to give them the best diet and exercise routines that work with 
+                        their indivual lifestyles and body types. Sign up down below for 
+                        a consultation and recieve your first month with us completely free!
+                    </p>
+                </Segment>
+            </div>
+            <div className='home-form'>
              <Form onSubmit={this.handleSubmit}>
                 <Form.Group widths='equal'>
                 <Form.Input
@@ -67,9 +71,15 @@ class Home extends Component {
                     placeholder='How did you hear about us?'
                     onChange={this.handleChange}
                 />
-                 <Form.Field control={Button}>Submit</Form.Field>
                 </Form.Group>
+                 <Button animated fluid color="blue">
+                    <Button.Content visible>Get my first month free!</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name="paper plane outline"/>
+                    </Button.Content>
+                </Button>
             </Form>
+            </div>
         </div>
     )
   }
