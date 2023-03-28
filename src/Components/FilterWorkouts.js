cfe095aa5b61b314aca7a8671517cd85daf75f93
Timothy <sanohoang@gmail.com>
2023-03-28 17:00:09 -0500

    cleaned up components

--- src/Components/FilterWorkouts.js
+++ src/Components/FilterWorkouts.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Checkbox } from 'semantic-ui-react';
+import React from "react";
+import { Checkbox } from "semantic-ui-react";
 
-const FilterWorkouts = ({  categories, toggleCategory, selected }) => {
-
+const FilterWorkouts = ({ categories, toggleCategory, selected }) => {
   const categoryButtons = categories.map((el) => (
     <div key={el} className="checkbox-wrapper">
-      <Checkbox 
-        label={el} 
-        onChange={() => toggleCategory(el)} 
-        checked={el === "All" ? selected.length === 0 || selected.includes("All") : selected.includes(el)}
+      <Checkbox
+        label={el}
+        onChange={() => toggleCategory(el)}
+        checked={
+          el === "All"
+            ? selected.length === 0 || selected.includes("All")
+            : selected.includes(el)
+        }
       />
     </div>
   ));
 
   return (
-    <div className='filters-container'> 
+    <div className="filters-container">
       <div className="checkbox">
         <h5>Filter</h5>
         {categoryButtons}
       </div>
     </div>
-  )
+  );
 };
 
 export default FilterWorkouts;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import teamLogo from "../images/teamLogo.png";
-import { Segment, Image, Button, Form, Select, Icon } from "semantic-ui-react";
+import { Image, Button, Form, Select, Icon } from "semantic-ui-react";
 
 const options = [
   { key: "f", text: "Facebook", value: "Facebook" },
@@ -23,23 +23,21 @@ class Home extends Component {
     return (
       <div className="home">
         <div className="home-bio">
-            <Image id="team-logo" src={teamLogo} size="large" centered />
-            <h2>About Us</h2>
-            <p>
-              Our gym was created by 3 extreme fitness competitors who grew a
-              passion for helping other people reach their goals too! We offer
-              group training classes, personal training sessions and we now have
-              an online based training, so you can get swoll in the comfort of
-              your own home! Each of our trainers have years of expierence and
-              their own unique specialties and training styles, giving our
-              clients a variety of different workouts to choose from. We take
-              pride in our training and ability to work one-on-one with clients
-              to give them the best diet and exercise routines that work with
-              their individual lifestyles and body types. Sign up down below for
-              a consultation and receive your first month with us completely
-              free!
-            </p>
-        
+          <Image id="team-logo" src={teamLogo} size="large" centered />
+          <h2>About Us</h2>
+          <p>
+            Our gym was created by 3 extreme fitness competitors who grew a
+            passion for helping other people reach their goals too! We offer
+            group training classes, personal training sessions and we now have
+            an online based training, so you can get swoll in the comfort of
+            your own home! Each of our trainers have years of expierence and
+            their own unique specialties and training styles, giving our clients
+            a variety of different workouts to choose from. We take pride in our
+            training and ability to work one-on-one with clients to give them
+            the best diet and exercise routines that work with their individual
+            lifestyles and body types. Sign up down below for a consultation and
+            receive your first month with us completely free!
+          </p>
         </div>
         <div className="home-form">
           <Form onSubmit={this.handleSubmit}>
@@ -67,7 +65,7 @@ class Home extends Component {
             </Form.Group>
             <Button id="home-button" animated fluid color="blue">
               <Button.Content visible>Get my first month free!</Button.Content>
-              <Button.Content hidden> 
+              <Button.Content hidden>
                 <Icon name="paper plane outline" /> Sent!
               </Button.Content>
             </Button>
