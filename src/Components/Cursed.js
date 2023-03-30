432a0815edeb2e9b12ee5336875a3dfda43f98fc
Brian Bissette <bbissette3@yahoo.com>
2023-03-30 16:31:15 -0500

    logo and trainer buttons fixed

--- src/Components/Cursed.js
+++ src/Components/Cursed.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import cursedImage from "../images/cursed.png"
+import React from 'react';
+import cursedImage from "../images/cursed.png";
+import { Image } from "semantic-ui-react";
 
 
 const Cursed=() => {
     return (
-        <div className='cursed'>
-            <h2 className='cursed-header'> This is how YOU can look in just 3 months with Swolldiers!
-                <img id="cursed-image"src={cursedImage}  alt="This-is-cursed"/>
-            </h2>
-        </div> 
+        <div className='cursed-container'>
+            <div className='cursed'>
+                <h2 className='cursed-header'> This is how YOU can look in just 3 months with Swolldiers!</h2>
+                    <Image src={cursedImage} alt="This-is-cursed" centered size="big"/>
+            </div> 
+        </div>
     )
 }
 
